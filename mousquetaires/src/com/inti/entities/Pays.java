2e8172dcfda30ae181a59414daaebe46ac0dba54
manselme <victor.bataillon@gmail.com>
2020-02-28 09:48:53 +0100

    ajouter l'interface serializable

--- mousquetaires/src/com/inti/entities/Pays.java
+++ mousquetaires/src/com/inti/entities/Pays.java
@@ -1,10 +1,12 @@
 package com.inti.entities;
 
+import java.io.Serializable;
+
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-public class Pays {
+public class Pays implements Serializable{
 	private static final long serialVersionUID = 1L;
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
