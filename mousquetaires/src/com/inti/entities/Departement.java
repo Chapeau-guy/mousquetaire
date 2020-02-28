a3d57e144735b53be7df7e02bfe116f04b644406
manselme <victor.bataillon@gmail.com>
2020-02-28 10:02:38 +0100

    ajouter l'interface serializable

--- mousquetaires/src/com/inti/entities/Departement.java
+++ mousquetaires/src/com/inti/entities/Departement.java
@@ -13,6 +13,32 @@ public class Departement implements Serializable{
 	private long idDepartement;
 	private String nomDepartement ;
 	private int numDepartement;
+	public long getIdDepartement() {
+		return idDepartement;
+	}
+	public void setIdDepartement(long idDepartement) {
+		this.idDepartement = idDepartement;
+	}
+	public String getNomDepartement() {
+		return nomDepartement;
+	}
+	public void setNomDepartement(String nomDepartement) {
+		this.nomDepartement = nomDepartement;
+	}
+	public int getNumDepartement() {
+		return numDepartement;
+	}
+	public void setNumDepartement(int numDepartement) {
+		this.numDepartement = numDepartement;
+	}
+	public Departement() {
+		super();
+	}
+	public Departement(String nomDepartement, int numDepartement) {
+		super();
+		this.nomDepartement = nomDepartement;
+		this.numDepartement = numDepartement;
+	}
 	
 	
 	
